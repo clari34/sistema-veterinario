54d6ff4b973e0fe644335f43465d53cfd5d42fcd
Renilson <86357654+renilsonjss@users.noreply.github.com>
2021-08-29 22:23:05 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    if (isset($_GET["view"])) 
+    {
+        require_once "app/Views/" . $_GET["view"] . ".php";
+    }
+    else if (isset($_GET["action"]) && isset($_GET["class"])) 
+    {
+        $controller = $_GET["class"] . "Controller";
+        $action = $_GET["action"];
+      
+        require_once "src/app/controllers/" . $controller . ".php";
+      
+        $controller = new $controller();
+        $controller->$action();
+    }
+?>
