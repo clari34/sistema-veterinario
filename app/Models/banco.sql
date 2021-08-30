434a848bcf64f2336f8145be6edc2d396fc9f571
Renilson <86357654+renilsonjss@users.noreply.github.com>
2021-08-30 01:09:36 -0300

    Update banco.sql

--- app/Models/banco.sql
+++ app/Models/banco.sql
@@ -3,9 +3,10 @@ USE veterinaria;
 
 CREATE TABLE usuarios
 (
-    id_usuario VARCHAR(14) PRIMARY KEY,
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    cpf VARCHAR(14) UNIQUE,
     nome VARCHAR(200),
-    email VARCHAR(200),
+    email VARCHAR(200) UNIQUE,
     senha VARCHAR(200)
 );
 
@@ -15,4 +16,16 @@ CREATE TABLE agendamento
     hora VARCHAR(13),
     vacina VARCHAR(20),
     CONSTRAINT usuario_agenda_fk FOREIGN KEY (ID_Pessoa) REFERENCES usuario (id_usuario)
+);
+
+CREATE TABLE veterinario
+(
+    id_veterinario INT AUTO_INCREMENT PRIMARY KEY,
+    cpf VARCHAR(14) UNIQUE,
+    nome VARCHAR(200),
+    email VARCHAR(200) UNIQUE,
+    senha VARCHAR(200),
+    usuario varchar(40) UNIQUE,
+    crmv varchar(20) UNIQUE,
+    email varchar(60) UNIQUE,
 );
